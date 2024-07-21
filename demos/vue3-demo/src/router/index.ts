6597a121e6c6e71b968f4b54ad5f127aaa386587
Alessandro-pang <pcy1314@163.com>
2024-07-21 16:02:12 +0800

    chore: 修复 github pages 发布后404的问题

--- demos/vue3-demo/src/router/index.ts
+++ demos/vue3-demo/src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 import PrintBasic from '../views/PrintBasic.vue'
 import PrintBasic2 from '../views/PrintBasicTwo.vue'
@@ -20,7 +20,7 @@ export const routes = [
 ]
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes,
 })
 
