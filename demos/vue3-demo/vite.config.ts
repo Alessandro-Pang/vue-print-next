bc35b2d67e9d4cbb27e98656126aa1208bf9e3b3
Alessandro-pang <pcy1314@163.com>
2024-07-21 15:57:43 +0800

    chore: 修复 github pages 发布后404的问题

--- demos/vue3-demo/vite.config.ts
+++ demos/vue3-demo/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig((conf) => {
+  const isProd = conf.mode === 'production';
+  return {
+    base: isProd ? '/vue-print-next/' :'/',
+    plugins: [vue()],
+  }
 })
